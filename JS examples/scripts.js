3dc55cf9a2dbd1d8728838349dba478cb2e82b7f
debanhian <debanhialanis16@gmail.com>
2017-04-08 10:40:41 -0500

    JS Ex

---
+++ JS examples/scripts.js
@@ -0,0 +1,7 @@
+function msg()  {
+
+  var text1= document.getElementById("textbox1").value;
+  var text2= document.getElementById("textbox2").value;
+var sumar= parseFloat(text1) + parseFloat(text2);
+  alert("La suma es: " + sumar);
+}
